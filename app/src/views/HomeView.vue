cbf2deeeea27b7e5e5a4114d683d08b08724eead
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-05-08 14:32:26 +0300

    tech

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,12 +2,14 @@
   <div>
     <Hero/>
     <Tech/>
+    <Client/>
   </div>
 </template>
 
 <script setup>
 import Hero from '../components/Hero.vue';
 import Tech from '../components/Tech.vue';
+import Client from '../components/Client.vue';
 </script>
 
 <style lang="scss" scoped>
