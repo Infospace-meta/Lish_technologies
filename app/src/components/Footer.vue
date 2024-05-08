045139808b1ce4497b7dad38c3ff359ee20f2313
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-05-08 22:51:29 +0300

    hero

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="flex flex-col p-6 bg-blue-950 max-w-full mx-auto">
-    <div class="w-full flex flex-col md:flex-row m-3 justify-evenly">
+    <div class="text-white font-bold text-9xl mt-44 text-center">Need IT Solutions? <br>Let's <span class="italic font-normal
+      ">start now.</span><br><br></div>
+
+      <div> <button class="mx-auto border border-white bg-blue-950 text-white font-bold py-2 px-4 rounded-md flex items-center justify-center">
+        GET FREE CONSULTATION
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      </button></div>
+    <div class="w-full flex flex-col md:flex-row m-3 justify-evenly mt-36">
       <!-- logo -->
       <div class="logo hidden w-1/4 sm:block">
         <img class="w-48" src="../assets/logo.png" alt="" />
