d9c02b3bf413ecb17fd26b63d389bfd4af263b78
ian <ndegwaian241@gmail.com>
2024-05-11 17:35:28 +0300

    message

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,110 +1,58 @@
 <template>
-  <div class="flex flex-col p-6 bg-blue-950 max-w-full mx-auto">
-    <div class="text-white font-bold text-4xl lg:text-9xl mt-44 text-center">
-      <span class="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-700">Need IT Solutions? </span><br />Let's
-      <span class="italic font-normal">start now.</span><br /><br />
-    </div>
+<body class="font-poppins bg-white">
 
-    <div>
-      <router-link to="/contact" class="mx-auto">
-      <button
-        class="mx-auto border border-white bg-blue-950 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center"
-      >
-        GET FREE CONSULTATION
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-arrow-right"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
-          />
-        </svg>
-      </button>
-    </router-link>
-    </div>
-    <div class="w-full flex flex-col md:flex-row m-3 justify-evenly mt-36">
-      <!-- logo -->
-      <div class="logo hidden w-1/4 sm:block">
-        <img class="w-48" src="../assets/logo.png" alt="" />
-      </div>
-
-      <div
-        class="cont_infor flex w-full justify-evenly text-white mx-auto flex-row items-start md:w-2/4"
-      >
-        <!-- navbar -->
-        <nav class="flex flex-col gap-2">
-          <ul>
-            <li>
-            <RouterLink to="/" class="text-purple-500">Home</RouterLink>
-          </li>
-          <li>
-                <RouterLink to="/about-us" class="hover:text-purple-500"
-                  >About us</RouterLink
-                >
-              </li>
-              <li>
-                <RouterLink to="/team" class="hover:text-purple-500"
-                  >Our team</RouterLink
-                >
-              </li>
-              <li>
-                <RouterLink to="/services" class="hover:text-purple-500"
-                  >Services</RouterLink
-                >
-              </li>
-              <li>
-                <RouterLink to="/contact" class="hover:text-purple-500"
-                  >Contact</RouterLink
-                >
-              </li>
-          </ul>
-        </nav>
-
-        <div class="info flex flex-col gap-3 ml-5">
-          <p>901-617-3690</p>
-          <h1>info@lish Tech.org</h1>
-          <h1>
-            Kenyatta Avenue,<br />
-            Gibcon House, 4th Floor <br />
-            Kenya,Nakuru
-          </h1>
+    <footer id="footer" class="bg-blue-950 g-center">
+        <div class="container mx-auto py-16 px-4">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div class="col-span-1">
+                    <div class="footer-about">
+                        <p>This is Lish empowering developers ecosystem</p>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="useful-link">
+                        <h2 class="text-2xl font-semibold pb-4 text-white">Useful Links</h2>
+                        <img src="./assets/images/about/home_line.png" alt="" class="img-fluid">
+                        <ul class="mt-4">
+                            <li><a href="#"><i class="fas fa-angle-right"></i> Home</a></li>
+                            <li><a href="#"><i class="fas fa-angle-right"></i> About Us</a></li>
+                            <li><a href="#"><i class="fas fa-angle-right"></i> Gallery</a></li>
+                            <li><a href="#"><i class="fas fa-angle-right"></i> Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="social-links">
+                        <h2 class="text-2xl font-semibold pb-4 text-white">Follow Us</h2>
+                        <img src="./assets/images/about/home_line.png" alt="">
+                        <ul class="mt-4">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i> Linkedin</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="address">
+                        <h2 class="text-2xl font-semibold pb-4 text-white">Address</h2>
+                        <img src="./assets/images/about/home_line.png" alt="" class="img-fluid">
+                        <ul class="mt-4">
+                            <li class="flex items-center"><i class="fas fa-location-dot"></i> Nakuru, Kenyatta Avenue, Gibcon House</li>
+                            <li><a href=""><i class="fas fa-phone"></i> +254 722 575 169</a></li>
+                            <li><a href=""><i class="fas fa-envelope"></i> info@lishlabs.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+    </footer>
 
-      <!-- div holding social media icons and the portfolio link -->
-      <div class="w-full mt-5 mx-auto flex flex-col justify-start md:w-1/4">
-        <!-- social media icons -->
-
-        <div class="w-1/2 mx-auto flex justify-between">
-          <img src="../assets/facebook.png" class="transition hover:scale-125" alt="" />
-          <img src="../assets/twitter.png" class="transition hover:scale-125" alt="" />
-          <img src="../assets/instagram.png" class="transition hover:scale-125" alt="" />
+    <section id="copy-right">
+        <div class="copy-right-sec bg-blue-950 text-white py-6 text-center">
+            <i class="fas fa-copyright"></i>
+            Lish Ai Labs 2024 Powerd By <a href="#" class="font-semibold">Infospace</a>
         </div>
-        <!-- the portfolio link -->
-        <div class="mt-3 w-1/2 mx-auto md:w-48">
-          <button
-            class="bg-white text-pink-900 w-full p-3 rounded hover:bg-pink-200"
-          >
-            <a href="#">View Portfolio</a>
-          </button>
-        </div>
-      </div>
-    </div>
-    <div
-      class="container mx-auto p-3 flex flex-col text-sm tracking-tighter text-white justify-between mt-10 md:flex-row"
-    >
-      <p class="">&copy;2024 All Rights Reserved| Lish Tech</p>
-      <p class=" ">Privacy Policy</p>
-      <p class="">Website Designed By InfoSpace</p>
-    </div>
-  </div>
+    </section>
+</body>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
