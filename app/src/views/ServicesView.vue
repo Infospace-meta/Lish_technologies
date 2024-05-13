e139372249fce3348ce2b03926f375ec8fffedec
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-05-13 12:50:16 +0300

    message

--- app/src/views/ServicesView.vue
+++ app/src/views/ServicesView.vue
@@ -2,12 +2,15 @@
     <div>
 <Solution/>
 <Service/>
+<Success/>
     </div>
 </template>
 
 <script setup>
 import Solution from '../components/Solution.vue';
 import Service from '../components/Service.vue';
+import Success from '../components/Success.vue';
+
 </script>
 
 <style lang="scss" scoped>
