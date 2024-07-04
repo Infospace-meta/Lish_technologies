18cad895c94a0defb771547a772ed9023beeba2c
ian723 <ndegwaian241@gmail.com>
2024-07-04 13:15:54 +0300

    message

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -1,6 +1,6 @@
 import HomeView from "./HomeView.vue";
 import DetailsView from "./DetailsView.vue";
-import LoginView from "./LoginView.vue";
+// import LoginView from "./LoginView.vue";
 import PostsView from "./PostsView.vue";
 import PiniaTest from "./PiniaTest.vue";
 import ContactView from "./ContactView.vue";
@@ -16,7 +16,7 @@ import NotFoundView from "./NotFoundView.vue";
 export {
   HomeView,
   DetailsView,
-  LoginView,
+  // LoginView,
   PostsView,
   PiniaTest,
   ContactView,
